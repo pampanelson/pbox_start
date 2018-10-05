6f6066bd6756e5dead06b0cbeaabf66be4b7d80c
pampa nelson <pampa.nelson@gmail.com>
2018-10-05 21:59:19 +0800

    full screen started

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 console.log("hello world hi there");
+
+
+window.onload = function() {
+
+    var toogleScreen = false;
+    var infoDiv = document.createElement('DIV');
+    var infoText = "info div: ";
+    var infoNode = document.createTextNode(infoText);
+
+    infoDiv.appendChild(infoNode);
+
+    var btn = document.createElement("BUTTON");
+    btn.innerHTML = "btn";
+    btn.addEventListener('click', function() {
+        toogleScreen = !toogleScreen;
+        if (toogleScreen) {
+            screenfull.exit();
+        } else {
+            screenfull.request();
+
+        }
+
+    })
+
+    document.body.appendChild(infoDiv);
+    document.body.appendChild(btn);
+
+
+
+}();
